eee61c997af7d113b3f0a2b645e95f111bdd64fe
godgiftedboy <rajprajapati12321@gmail.com>
2025-04-07 17:11:21 +0545

    chore: added UIAlertController and added a UIAlertAction with handler as a closure with weak ref with ViewController and UIAlertContrller

--- project5/ViewController.swift
+++ project5/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 class ViewController: UITableViewController {
     var allWords = [String]()
     var usedWords = [String]()
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
+        
         if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
             if let startWords = try? String(contentsOf: startWordsURL,encoding: .utf8) {
                 allWords = startWords.components(separatedBy: "\n")
@@ -40,8 +45,34 @@ class ViewController: UITableViewController {
         cell.textLabel?.text = usedWords[indexPath.row];
         return cell;
     }
+    //It needs to be called from a UIBarButtonItem action, so we must mark it @objc.
+    //Hopefully you’re starting to sense when this is needed.
     
+    //As UIBarButtonItem is from Core which is writted in Objective-C rather than Swift
+    //To expose it to UIBarButtonItem, @objc was added to Swift function
+    
+    //But don’t worry if you forget – Xcode will always complain loudly if @objc is required and not present!
+    @objc func promptForAnswer() {
+        let ac = UIAlertController(title: "Enter answer", message: nil, preferredStyle: .alert)
+        ac.addTextField()
 
+        let submitAction = UIAlertAction(title: "Submit", style: .default) {
+            //Both ViewController and AlertController are referenced inside our closure.
+            //So, weak reference should be indicated.
+        
+            [weak self, weak ac] action in
+            guard let answer = ac?.textFields?[0].text else { return } // AlertController referenced (ac)
+            self?.submit(answer) // ViewController referenced (self)
+            
+            //So, question marks (?.) are there due to weak references established.
+        }
+
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
+    func submit(_ answer: String) {
+    }
+    
 
 }
 
