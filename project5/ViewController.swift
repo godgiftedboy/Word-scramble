e5f2c61a8e002e4217096b4fd906ea04913267f0
godgiftedboy <rajprajapati12321@gmail.com>
2025-04-08 09:31:02 +0545

    chore: added error message presentation on invalid word entry

--- project5/ViewController.swift
+++ project5/ViewController.swift
@@ -72,6 +72,9 @@ class ViewController: UITableViewController {
     }
     func submit(_ answer: String) {
         let lowerAnswer = answer.lowercased()
+        
+        let errorTitle: String
+           let errorMessage: String
 
             if isPossible(word: lowerAnswer) {
                 if isOriginal(word: lowerAnswer) {
@@ -79,9 +82,23 @@ class ViewController: UITableViewController {
                         usedWords.insert(answer, at: 0)
                         let indexPath = IndexPath(row: 0, section: 0)
                         tableView.insertRows(at: [indexPath], with: .automatic)
+                        return
+                    }else {
+                        errorTitle = "Word not recognised"
+                        errorMessage = "You can't just make them up, you know!"
                     }
+                }else {
+                    errorTitle = "Word used already"
+                    errorMessage = "Be more original!"
                 }
+            }else {
+                guard let title = title?.lowercased() else { return }
+                errorTitle = "Word not possible"
+                errorMessage = "You can't spell that word from \(title)"
             }
+        let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
     }
     
     //check with the given word in title
